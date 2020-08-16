f4be1ff23b63859b5a0d7d41e4eb7cc4a1db6068
CodeWithGamez <codewithgamez@gmail.com>
2020-08-16 21:20:16 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+#Write code for using VARIABLES AND TYPES
+
+
+
+#1 Make a variable named hobby and store a string of your favortie hobby and print it
+
+
+
+
+
+
+
+#2 Make a variable named hours and store a number of how long you spend doing your favorite hobby and print it
+
+
+
+
+
+
+
+
+#3 Print the what types of data your variables of hobby and hours are 
+
+
+
